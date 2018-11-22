0883da9a2893419e02fc3abe1034cb611064f970
TTNguyen <nttriet9a4@gmail.com>
2018-11-22 10:02:33 +0700

    Initial Commit

---
+++ Instagram_withStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram_withStoryBoard
+//
+//  Created by TT Nguyen on 11/22/18.
+//  Copyright © 2018 TT Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
