5d99fd46d7966f4cf588c94e80dacc43b47c3e3a
TTNguyen <nttriet9a4@gmail.com>
2018-12-29 17:11:48 +0700

    Fix bug

---
+++ Instagram_withStoryBoard/API/AuthApi.swift
@@ -0,0 +1,9 @@
+//
+//  AuthApi.swift
+//  Instagram_withStoryBoard
+//
+//  Created by TT Nguyen on 12/29/18.
+//  Copyright © 2018 TT Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ Instagram_withStoryBoard/Helper/CustomAlert .swift
@@ -0,0 +1,9 @@
+//
+//  Alert .swift
+//  Instagram_withStoryBoard
+//
+//  Created by TT Nguyen on 12/29/18.
+//  Copyright © 2018 TT Nguyen. All rights reserved.
+//
+
+import Foundation
